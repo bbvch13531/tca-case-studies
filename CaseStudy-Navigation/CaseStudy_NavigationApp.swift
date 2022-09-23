172f1ce0dbf2b068d4445f731f5c91afcc963e8a
Heo kyuyoung <bbvch13531@gmail.com>
2022-09-23 22:46:15 +0900

    Initial Commit

---
+++ CaseStudy-Navigation/CaseStudy_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseStudy_NavigationApp.swift
+//  CaseStudy-Navigation
+//
+//  Created by ky on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CaseStudy_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
